8bd0a378609209b07ddc09ac83273e226e2f7336
GoldFlsh <Ryan.Z.Khalil@gmail.com>
2018-09-20 17:59:06 -0500

    Initial Commit of start.spring.io gradle project

---
+++ src/main/java/org/ryank/pizza/demopizzaorder/DemoPizzaOrderApplication.java
@@ -0,0 +1,12 @@
+package org.ryank.pizza.demopizzaorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPizzaOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPizzaOrderApplication.class, args);
+	}
+}
